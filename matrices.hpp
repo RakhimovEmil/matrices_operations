71d0ce83b338491e53575542d731c8fd4b89efb1
RakhimovEmil <69158088+RakhimovEmil@users.noreply.github.com>
2022-01-22 19:16:39 +0300

    Update matrices.hpp

--- matrices.hpp
+++ matrices.hpp
@@ -1,15 +1,15 @@
 #pragma once
-#include <iostream>
+#include <cstddef>
 
-bool if_add_or_mult(char *my_string);
+bool if_add_or_mult(char const* my_string);
 
 void memory_clean(float** memory, size_t size);
 
-float ** get_matrix_from_file(char const *file_name_a, std::size_t *size_row, std::size_t *size_column);
+float** get_matrix_from_file(char const *file_name_a, std::size_t& size_row, std::size_t& size_column);
 
 void get_sum(float **array_a, float **array_b,
-            std::size_t row_a, std::size_t column_a);
+             std::size_t row_a, std::size_t column_a);
 
 void get_mult(float **array_a, float **array_b,
-             std::size_t row_a, std::size_t *column_a,
-             std::size_t row_b, std::size_t column_b);
+              std::size_t row_a, std::size_t& column_a,
+              std::size_t row_b, std::size_t column_b);
